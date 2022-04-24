3242889b30a21df69d678f43f7fd7a7dec798514
Bradley Marques <bradleyrcmarques@gmail.com>
2022-04-24 20:28:49 +0200

    Uses environment variables

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -5,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=[os.environ.get("FRONTEND_URL")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
