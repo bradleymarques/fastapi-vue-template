fc5b833ed9dfebfb05d1771bdf6d865baded3a88
Bradley Marques <bradleyrcmarques@gmail.com>
2022-04-29 07:24:36 +0200

    Uses Tortoise as ORM with simple model

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,8 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from src.database.register import register_tortoise
+from src.database.config import TORTOISE_ORM
 
 
 app = FastAPI()
@@ -11,6 +13,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
 
 
 @app.get("/")
